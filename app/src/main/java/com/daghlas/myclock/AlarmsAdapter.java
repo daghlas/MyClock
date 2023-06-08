89e5efdc1103f19a20d88d51eddb29eef654fefd
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-06-08 13:42:11 +0300

    initial commit

--- app/src/main/java/com/daghlas/myclock/AlarmsAdapter.java
+++ app/src/main/java/com/daghlas/myclock/AlarmsAdapter.java
@@ -17,6 +17,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * Created by Daghlas Kaire
+ * Github: https://github.com/daghlas
+ * Mail: daghlaskaire58@gmail.com
+ */
+
 public class AlarmsAdapter extends RecyclerView.Adapter<AlarmsAdapter.MyViewHolder> {
 
     Context context;
